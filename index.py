b00e8f0c55ed235d7a5f672a412c9e1df72415a5
Mauro Alvarado <mauro_alvarado_rdz@hotmail.com>
2020-10-27 13:58:56 -0600

    todo bien hasta aqui 20:55

--- index.py
+++ index.py
@@ -9,6 +9,20 @@ class Product:
     self.wind = window
     self.wind.title('Productos')
 
+    #contenedor/frame
+    frame = LabelFrame(self.wind, text = 'registra un nuevo produco')
+    frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+
+    #Name input
+    Label(frame, text = 'Nombre: ').grid(row = 1, column = 0)
+    self.name = Entry(frame)
+    self.name.grid(row = 1, column = 1)
+
+    #price imput
+    Label(frame, text = 'Precio: ').grid(row = 2, column = 0)
+    self.price = Entry(frame)
+    self.price.grid(row = 2, column = 1)
+
 if __name__ == "__main__":
   window = Tk()
   application = Product(window)
