b4b7f9796e09bd84637f88a5b07086364841bcfe
Mauro Alvarado <mauro_alvarado_rdz@hotmail.com>
2020-10-27 13:25:40 -0600

    hasta aqui se puede abrir una ventana vacia

---
+++ index.py
@@ -0,0 +1,15 @@
+from tkinter import ttk 
+from tkinter import *
+
+import sqlite3
+
+class Product:
+
+  def __init__(self, window):
+    self.wind = window
+    self.wind.title('Productos')
+
+if __name__ == "__main__":
+  window = Tk()
+  application = Product(window)
+  window.mainloop()
